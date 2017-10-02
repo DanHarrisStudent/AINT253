2dd97eddc69109fffe96f02ed8802cc39bfc57ed
DanHarrisStudent <32270584+DanHarrisStudent@users.noreply.github.com>
2017-10-02 17:47:01 +0100

    10.02.2017 Inclass work

--- DoorPresentation/Assets/Scripts/DoorCollision.cs
+++ DoorPresentation/Assets/Scripts/DoorCollision.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class DoorCollision : MonoBehaviour {
 
-    private Animator _animator;
+    private Animator m_animator;
+    private AudioSource m_sound;
 
     void Start()
     {
-        _animator = GetComponent<Animator>();
+        m_animator = GetComponent<Animator>();
+        m_sound = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-        _animator.SetBool("open", true);
+            m_animator.SetBool("open", true);
+            m_sound.Play();
         }
     }
 }
