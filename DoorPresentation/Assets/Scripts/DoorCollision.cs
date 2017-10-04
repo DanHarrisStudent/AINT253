e0b33c7164ab8b64fb1fab51fce1ff1d30de7a9e
Dan-PC\Dan <daniel.harris-4@students.plymouth.ac.uk>
2017-10-04 11:53:51 +0100

    sync

--- DoorPresentation/Assets/Scripts/DoorCollision.cs
+++ DoorPresentation/Assets/Scripts/DoorCollision.cs
@@ -2,20 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorCollision : MonoBehaviour {
+public class DoorCollision : MonoBehaviour
+{
 
     private Animator _animator;
+    private AudioSource _audio;
 
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _audio = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
-        _animator.SetBool("open", true);
+            _animator.SetBool("open", true);
+            _audio.Play();
         }
     }
 }
