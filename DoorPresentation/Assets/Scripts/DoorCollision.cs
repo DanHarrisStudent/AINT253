6f17d28e3f04950e52e819eef87ae97b3025007f
DanHarrisStudent <32270584+DanHarrisStudent@users.noreply.github.com>
2017-10-06 10:58:10 +0100

    06/10/2017
    proof of creating a door

--- DoorPresentation/Assets/Scripts/DoorCollision.cs
+++ DoorPresentation/Assets/Scripts/DoorCollision.cs
@@ -5,15 +5,8 @@ using UnityEngine;
 public class DoorCollision : MonoBehaviour
 {
 
-<<<<<<< HEAD
     private Animator _animator;
     private AudioSource _audio;
-
-    void Start()
-    {
-        _animator = GetComponent<Animator>();
-        _audio = GetComponent<AudioSource>();
-=======
     private Animator m_animator;
     private AudioSource m_sound;
 
@@ -21,20 +14,17 @@ public class DoorCollision : MonoBehaviour
     {
         m_animator = GetComponent<Animator>();
         m_sound = GetComponent<AudioSource>();
->>>>>>> b6f1b798b06b72feb962b88218f485b44748a764
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-<<<<<<< HEAD
             _animator.SetBool("open", true);
             _audio.Play();
-=======
+
             m_animator.SetBool("open", true);
             m_sound.Play();
->>>>>>> b6f1b798b06b72feb962b88218f485b44748a764
         }
     }
 }
